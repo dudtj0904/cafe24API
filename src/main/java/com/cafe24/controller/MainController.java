f366946f1b1b7618fe54c259493fc89f29c13caa
bit <bit@DESKTOP-28T4BRB>
2018-06-25 11:13:45 +0900

    1

---
+++ src/main/java/com/cafe24/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.cafe24.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// @RestController : data return
+// -> 객체를 반환하면 객체 데이터는 JSON/XML 형식의 HTTP 응답 작성
+// -> @Controller + @ResponseBody
+@Controller		// : view(화면) return
+public class MainController {
+
+	@RequestMapping(value="/index")
+	public String main(Model model) {
+		model.addAttribute("hello", "hello hello");
+		
+		return "index";	// /WEB-INF/views/index.jsp
+	}
+	
+	@RequestMapping(value="/index2")
+	public String main2(Model model) {
+		model.addAttribute("hello", "hello hello2");
+		
+		return "index";	// /WEB-INF/views/index.jsp
+	}
+
+}
---
+++ src/main/java/com/cafe24/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.cafe24.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	/**
+	 * Security Config Class
+	 */
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 모든 페이지 비인증 설정
+		// web.ignoring().antMatchers("/**");
+		
+		web.ignoring().antMatchers("/css/**", "/script/**", "/");
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// resource 외의 페이지의 권한 설정
+		// http.authorizeRequests().antMatchers("/admin/**").access("ROLE_ADMIN");
+		
+		// localhost:8080/index 는 인증된 사용자만이 접속 가능하도록 설정
+		/* http.authorizeRequests().antMatchers("/index").authenticated()
+		.antMatchers("/**").permitAll(); */
+	}
+	
+	
+
+	
+	
+
+}
