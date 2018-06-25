ea7769ba82de010f6c6d624bef253e50c675f8d7
bit <bit@DESKTOP-28T4BRB>
2018-06-25 17:45:48 +0900

    dfsf

--- src/main/java/com/cafe24/Cafe24ApiApplication.java
+++ src/main/java/com/cafe24/Cafe24ApiApplication.java
@@ -1,11 +1,16 @@
 package com.cafe24;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
 public class Cafe24ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
