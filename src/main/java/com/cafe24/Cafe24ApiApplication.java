66de41a6d5e81fa8b3b96ebe1c1fb6a185c21206
bit <bit@DESKTOP-28T4BRB>
2018-06-25 15:12:56 +0900

    cf

--- src/main/java/com/cafe24/Cafe24ApiApplication.java
+++ src/main/java/com/cafe24/Cafe24ApiApplication.java
@@ -2,11 +2,18 @@ package com.cafe24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cafe24ApiApplication {
+public class Cafe24ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cafe24ApiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cafe24ApiApplication.class);
 	}
 }
